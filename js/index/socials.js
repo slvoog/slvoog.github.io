73eb42c232de5d45072086c31ebdc23c34b444c7
svloog <78726502+slvoog@users.noreply.github.com>
2022-05-09 12:56:10 +0300

    Upload complete site

---
+++ js/index/socials.js
@@ -0,0 +1,39 @@
+function githubHover(boolean) {
+    if (boolean == true) {
+        document.getElementById("name").innerHTML = "github";
+    } else if (boolean == false) {
+        document.getElementById("name").innerHTML = "svloogz";
+    }
+}
+
+function projectsHover(boolean) {
+    if (boolean == true) {
+        document.getElementById("name").innerHTML = "projects";
+    } else if (boolean == false) {
+        document.getElementById("name").innerHTML = "svloogz";
+    }
+}
+
+function emailHover(boolean) {
+    if (boolean == true) {
+        document.getElementById("name").innerHTML = "email";
+    } else if (boolean == false) {
+        document.getElementById("name").innerHTML = "svloogz";
+    }
+}
+
+function discordHover(boolean) {
+    if (boolean == true) {
+        document.getElementById("name").innerHTML = "discord";
+    } else if (boolean == false) {
+        document.getElementById("name").innerHTML = "svloogz";
+    }
+}
+
+function twitterHover(boolean) {
+    if (boolean == true) {
+        document.getElementById("name").innerHTML = "twitter";
+    } else if (boolean == false) {
+        document.getElementById("name").innerHTML = "svloogz";
+    }
+}
